85c7f2cbaed9f149fde42e66dd8f1aefbb0e8d2a
huangjiarui <>
2018-06-20 11:27:27 +0800

    project init commit

---
+++ src/main/java/com/mmall/dao/ShippingMapper.java
@@ -0,0 +1,30 @@
+package com.mmall.dao;
+
+import com.mmall.pojo.Shipping;
+import com.mmall.pojo.ShippingExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface ShippingMapper {
+    int countByExample(ShippingExample example);
+
+    int deleteByExample(ShippingExample example);
+
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Shipping record);
+
+    int insertSelective(Shipping record);
+
+    List<Shipping> selectByExample(ShippingExample example);
+
+    Shipping selectByPrimaryKey(Integer id);
+
+    int updateByExampleSelective(@Param("record") Shipping record, @Param("example") ShippingExample example);
+
+    int updateByExample(@Param("record") Shipping record, @Param("example") ShippingExample example);
+
+    int updateByPrimaryKeySelective(Shipping record);
+
+    int updateByPrimaryKey(Shipping record);
+}
---
+++ src/main/java/com/mmall/util/OverIsMergeablePlugin.java
@@ -0,0 +1,51 @@
+package com.mmall.util;
+
+import org.mybatis.generator.api.GeneratedXmlFile;
+import org.mybatis.generator.api.IntrospectedTable;
+import org.mybatis.generator.api.MyBatisGenerator;
+import org.mybatis.generator.api.PluginAdapter;
+import org.mybatis.generator.config.Configuration;
+import org.mybatis.generator.config.xml.ConfigurationParser;
+import org.mybatis.generator.internal.DefaultShellCallback;
+import sun.tools.native2ascii.Main;
+
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @title mybatis逆向工程重新生成数据结构
+ * @Author huangjiarui
+ * @date: 2018-06-19
+ */
+public class OverIsMergeablePlugin extends PluginAdapter {
+    @Override
+    public boolean validate(List<String> warnings) {
+        return true;
+    }
+
+    @Override
+    public boolean sqlMapGenerated(GeneratedXmlFile sqlMap, IntrospectedTable introspectedTable) {
+        try {
+            Field field = sqlMap.getClass().getDeclaredField("isMergeable");
+            field.setAccessible(true);
+            field.setBoolean(sqlMap, false);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return true;
+    }
+
+    public static void main(String[] args) throws Exception {
+        List<String> warnings = new ArrayList<String>();
+        boolean overwrite = true;
+        ConfigurationParser cp = new ConfigurationParser(warnings);
+        Configuration config = cp.parseConfiguration(Main.class.getClassLoader().getResourceAsStream("generatorConfig.xml"));
+        DefaultShellCallback callback = new DefaultShellCallback(overwrite);
+        MyBatisGenerator myBatisGenerator = new MyBatisGenerator(config, callback, warnings);
+        myBatisGenerator.generate(null);
+        System.out.println("----recycle is ok----");
+    }
+}
+
+
